62150b45783624db26847a911097f620ba7af179
Muhammad-Abid11 <63147718+Muhammad-Abid11@users.noreply.github.com>
2022-04-15 16:43:55 +0500

    .

--- app.js
+++ app.js
@@ -20,3 +20,23 @@ function cal(clicked_id) {
 
 
 }
+
+var val = document.getElementById("inp");
+
+function AT_add(v) {
+    val.value += v;
+}
+
+
+function cls() {
+    val.value = "";
+}
+
+function exe() {
+    val.value = eval(val.value);
+}
+
+
+function cancel() {
+    val.value = val.value.substr(0, val.value.length - 1);
+}
