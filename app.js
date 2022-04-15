1868e234fc2ab1cf8abe14f8a90626be38907b44
Muhammad-Abid11 <63147718+Muhammad-Abid11@users.noreply.github.com>
2022-04-15 16:41:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// cal = (a) => {
+//     console.log("this working")
+//     console.log(a)
+// }
+
+// $("button").click(function () {
+//     var button = $(this).val();
+//     alert(button);
+// });
+
+display = document.getElementById(display);
+
+function cls() {
+    display.value = "";
+}
+
+
+function cal(clicked_id) {
+    console.log(clicked_id);
+
+
+}
